3047369715320e3ae4161e443b671a8135f8da06
Stevent Rangga <prayoganabiel74@gmail.com>
2023-03-21 16:23:19 +0700

    commit penjumlahan

--- modul5_1302210049/Program.cs
+++ modul5_1302210049/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+public class program
+{
+    public static void Main(String[] args)
+    {
+        Penjumlahan hitung = new Penjumlahan();
+        Console.WriteLine(hitung.JumlahTigaAngka<int>(13, 02, 21));
+    }
+}
+public class Penjumlahan
+{
+    public T JumlahTigaAngka<T>(T input1, T input2, T input3)
+    {
+        dynamic number1 = input1;
+        dynamic number2 = input2;
+        dynamic number3 = input3;
+        return number1 + number2 + number3;
+    }
+}
